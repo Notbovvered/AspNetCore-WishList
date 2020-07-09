c06862f328fae910accdd81a8e86a6c5467045a3
Joseph <Jkhassan95@gmail.com>
2020-07-09 16:44:30 +0100

    Code for part 6

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
@@ -12,7 +14,24 @@ namespace WishList.Controllers
         private readonly ApplicationDbContext _context;
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
+            
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Item()
+        {
+            return View("Item");
+        }
+
+        public IActionResult Delete()
+        {
+            return View("Delete");
         }
     }
 }
